7fb3ebe01b59b596e2d22f56e6d57c8457568999
roman.nekrasov@rtlabs.ru <roman.nekrasov@rtlabs.ru>
2017-10-26 16:58:02 +0300

    version "1.0"

---
+++ src/main/java/r/test/RFile.java
@@ -0,0 +1,15 @@
+package r.test;
+
+public class RFile {
+    private String name;
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
---
+++ src/main/java/r/test/RFileList.java
@@ -0,0 +1,18 @@
+package r.test;
+
+import java.util.*;
+
+public class RFileList {
+
+       ArrayList<RFile> List;
+       
+       RFileList () {
+       	   List = new ArrayList<RFile>();
+       }
+       
+       RFileList (ArrayList<RFile> l) {
+           this.List = l;
+       }
+       
+}
+	
